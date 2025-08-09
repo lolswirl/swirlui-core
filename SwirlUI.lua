0d78ed213caad330fe3574a387db46936229e7b1
swirl <180882683+lolswirl@users.noreply.github.com>
2025-08-09 14:55:59 -0400

    [feature] add slash command `/swirlui`

--- SwirlUI.lua
+++ SwirlUI.lua
@@ -2,6 +2,6 @@ local _, SwirlUI = ...
 local SUI = LibStub("AceAddon-3.0"):NewAddon("SwirlUI")
 
 function SUI:OnEnable()
-    print("|cff02fd98Swirl|rUI Core Loaded")
+    print("|cff02fd98Swirl|rUI Core » |cff02fd98/swirlui|r for config")
     SwirlUI:Initialize()
 end
